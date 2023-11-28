1ccb8e8bf66d9718651267b4a2eaaff6324bd572
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-11-28 02:04:05 -0800

    isAdmin false

--- models/user.js
+++ models/user.js
@@ -7,7 +7,8 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  secrets: [SecretSchema]
+  secrets: [SecretSchema],
+  isAdmin: { type: Boolean, default: false }
 });
 
 // Export model
